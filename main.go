281ba97b24060a47ef2a56c55ac03e2e6ed0ee56
Paolo Fabbri <jabawack81@gmail.com>
2025-04-13 14:06:26 +0100

    init

---
+++ main.go
@@ -0,0 +1,90 @@
+ package main
+ 
+
+ import (
+  "fmt"
+  "log"
+  "net/url"
+  "os"
+  "os/exec"
+  "strings"
+ 
+
+  "github.com/go-git/go-git/v5"
+  // "github.com/go-git/go-git/v5/config"
+  // "github.com/go-git/go-git/v5/plumbing"
+ )
+ 
+
+ func main() {
+  repo, err := git.PlainOpen(".")
+  if err != nil {
+  log.Fatalf("Failed to open git repository: %v", err)
+  }
+ 
+
+  remoteName := "origin"
+  remote, err := repo.Remote(remoteName)
+  if err != nil {
+  log.Fatalf("Failed to get remote '%s': %v", remoteName, err)
+  }
+ 
+
+  remoteURL := remote.Config().URLs[0]
+  githubURL, err := parseGithubURL(remoteURL)
+  if err != nil {
+  log.Fatalf("Failed to parse GitHub URL: %v", err)
+  }
+ 
+
+  headRef, err := repo.Head()
+  if err != nil {
+  log.Fatalf("Failed to get HEAD reference: %v", err)
+  }
+  currentBranch := headRef.Name().Short()
+ 
+
+  newPRURL := fmt.Sprintf("%s/compare/%s", githubURL, url.PathEscape(currentBranch))
+ 
+
+  fmt.Printf("Opening: %s\n", newPRURL)
+  openBrowser(newPRURL)
+ }
+ 
+
+ func parseGithubURL(remoteURL string) (string, error) {
+  if strings.HasPrefix(remoteURL, "git@") {
+  // SSH URL format: git@github.com:owner/repo.git
+  parts := strings.SplitN(remoteURL, ":", 2)
+  if len(parts) < 2 {
+  return "", fmt.Errorf("invalid SSH remote URL format")
+  }
+  repoPath := strings.TrimSuffix(parts[1], ".git")
+  return fmt.Sprintf("https://github.com/%s", repoPath), nil
+  } else if strings.HasPrefix(remoteURL, "http://") || strings.HasPrefix(remoteURL, "https://") {
+  // HTTP/HTTPS URL format: https://github.com/owner/repo.git
+  trimmedURL := strings.TrimSuffix(remoteURL, ".git")
+  return trimmedURL, nil
+  } else {
+  return "", fmt.Errorf("unsupported remote URL format")
+  }
+ }
+ 
+
+ func openBrowser(url string) {
+  var err error
+ 
+
+  switch os := os.Getenv("OS"); os {
+  case "windows":
+  err = exec.Command("cmd", "/c", "start", url).Run()
+  case "darwin":
+  err = exec.Command("open", url).Run()
+  default: // "linux", "freebsd", "openbsd", "netbsd"
+  err = exec.Command("xdg-open", url).Run()
+  }
+  if err != nil {
+  log.Fatalf("Failed to open browser: %v", err)
+  }
+ }
+

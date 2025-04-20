f42b6caf3d3a8f4b5c90ea3062178462b73e78fd
Fabbri Paolo <jabawack81@gmail.com>
2025-04-20 18:15:32 +0100

    Update main.go with license info

--- main.go
+++ main.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2024 Internet Blacksmith
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
 package main
 
 import (
